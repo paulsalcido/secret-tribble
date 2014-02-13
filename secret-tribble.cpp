7863509a7da04d7cba6a0a18ba12cd56f5d70f4b
Paul Salcido <paulsalcido.79@gmail.com>
2014-02-13 18:44:37 -0500

    Avoid core dump with nonexistent lesson

--- secret-tribble.cpp
+++ secret-tribble.cpp
@@ -5,7 +5,9 @@
 
 int main(int argc, char *argv[]) {
     st::app *main_app = st::app::build_app(argc > 1 ? argv[1] : "sdl");
-    main_app->run();
-    delete main_app;
+    if ( main_app != NULL ) {
+        main_app->run();
+        delete main_app;
+    }
     return 0;
 }
